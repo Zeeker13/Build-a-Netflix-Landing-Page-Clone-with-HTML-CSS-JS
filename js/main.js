ea3d3286002e56826398014f1e60b07e28222024
Ashen Prageeth Hewavitharana <ashenprageeth1@gmail.com>
2024-01-15 12:13:45 +0530

    Finished
    all done

--- js/main.js
+++ js/main.js
@@ -7,11 +7,23 @@ tabItems.forEach(item=>item.addEventListener('click',selectItem));
 
 function selectItem(e){
     removeBorder();
+    removeshow();
     this.classList.add('tab-border');
+    //grab content item from the DOM
+
+    const tabContentItems = document.querySelector(`#${this.id}-content`);
+    tabContentItems.classList.add('show')
+    
+
 }
 
 function removeBorder(){
     tabItems.forEach(item=>item.classList.remove('tab-border'));
 }
+
+function removeshow(){
+    tabContentItems.forEach(item=>item.classList.remove('show'));
+}
+
 
 tabItems.forEach(item=>item.addEventListener('click',selectItem));
