49c92b6a522ce6990c29f32e5708fbc7f4c38b49
Ashen Prageeth Hewavitharana <ashenprageeth1@gmail.com>
2024-01-15 10:10:57 +0530

    up to js
    up to js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+tabItems.forEach(item=>item.addEventListener('click',selectItem));
+
+function selectItem(e){
+    removeBorder();
+    this.classList.add('tab-border');
+}
+
+function removeBorder(){
+    tabItems.forEach(item=>item.classList.remove('tab-border'));
+}
+
+tabItems.forEach(item=>item.addEventListener('click',selectItem));
